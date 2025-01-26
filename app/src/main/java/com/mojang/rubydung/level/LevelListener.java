6cd94b8b4ed9cef28c17a89e339c54d0634ddd59
Levviata <68709348+Levviata@users.noreply.github.com>
2025-01-26 18:41:35 +0100

    Add Minecraft rd-132211 source code to be worked from

---
+++ app/src/main/java/com/mojang/rubydung/level/LevelListener.java
@@ -0,0 +1,9 @@
+package com.mojang.rubydung.level;
+
+public interface LevelListener {
+    void tileChanged(int paramInt1, int paramInt2, int paramInt3);
+    
+    void lightColumnChanged(int paramInt1, int paramInt2, int paramInt3, int paramInt4);
+
+    void allChanged();
+}
